ee613c9416da4e92fc9434adbc5507e9530931a7
hir0github <almazdautov1234523@gmail.com>
2022-04-22 22:33:58 +0300

    1

--- src/EmergencyRequest.java
+++ src/EmergencyRequest.java
@@ -1,2 +1,14 @@
-public class EmergencyRequest {
+public class EmergencyRequest extends Request {
+
+    public EmergencyRequest(Position position, int clientID, GoggleMaps Gogglemaps){
+        this.position = position;
+        this.clientID = clientID;
+        this.hospitalName = Gogglemaps.getNearestHospitalName(position);
+        this.car = Vehicle.any;
+        sendRequest(server);
+    }
+    @Override
+    boolean sendRequest(Server server){
+        return server.manageEmergencyRequest(this);
+    }
 }
--- src/Request.java
+++ src/Request.java
@@ -1,3 +1,18 @@
 public class Request {
+
     public Vehicle car;
+    public Position position;
+    public String hospitalName;
+    public int clientID;
+
+    public Request(){}
+    public Request(Position position, Vehicle car, String hospitalName, int clientID){
+        this.car = car;
+        this.position = position;
+        this.hospitalName = hospitalName;
+        this.clientID = clientID;
+    }
+    boolean sendRequest(Server server){
+        return server.manageRequest(this);
+    }
 }
