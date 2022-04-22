a5e4583f682aa598777056846c0c4fff58673877
Dirakon <609700zxc@mail.ru>
2022-04-22 22:00:03 +0300

    Added Hospital + HospitalList + GoggleMaps; Position fix

--- src/GoggleMaps.java
+++ src/GoggleMaps.java
@@ -1,2 +1,35 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class GoggleMaps {
+    private HospitalList hospitals;
+
+    public GoggleMaps(HospitalList hospitals) {
+        this.hospitals = hospitals;
+    }
+
+    public List<String> getAllHospitalInfo() {
+        List<String> info = new LinkedList<>();
+        for (Hospital h : hospitals.getHospitals()) {
+            info.add(h.getName() + ": " + h.getPosition());
+        }
+        return info;
+    }
+
+    public String getNearestHospitalName(Position position) {
+        if (hospitals.getHospitals().isEmpty()) {
+            throw new IllegalArgumentException("No hospitals in the list, cannot find the nearest one");
+        }
+        double minDistance = Double.MAX_VALUE;
+        String nearestHospital = "";
+        for (Hospital hospitalToConsider : hospitals.getHospitals()) {
+            double distance = position.getDistanceTo(hospitalToConsider.getPosition());
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestHospital = hospitalToConsider.getName();
+            }
+        }
+        return nearestHospital;
+    }
+
 }
