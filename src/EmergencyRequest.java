7156a0eeef770ebeca586c114457d716e141a666
starkda <starkizi47@mail.ru>
2022-04-25 21:46:49 +0300

    bug fix

--- src/EmergencyRequest.java
+++ src/EmergencyRequest.java
@@ -1,14 +1,2 @@
-public class EmergencyRequest extends Request {
-
-    public EmergencyRequest(Position position, int clientID, GoggleMaps Gogglemaps){
-        this.position = position;
-        this.clientID = clientID;
-        this.hospitalName = Gogglemaps.getNearestHospitalName(position);
-        this.car = Vehicle.any;
-        sendRequest(server);
-    }
-    @Override
-    boolean sendRequest(Server server){
-        return server.manageEmergencyRequest(this);
-    }
+public class EmergencyRequest extends Request{
 }
